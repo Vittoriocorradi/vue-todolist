7ddf1f5f3e7139612eb647d8cabd3e164c576359
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-03-15 15:20:33 +0100

    Lista oggetti e struttura html

--- js/main.js
+++ js/main.js
@@ -3,7 +3,24 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-
+            todoList: [
+                {
+                    text: 'Comprare il pane',
+                    done: false
+                },
+                {
+                    text: 'Dare da mangiare alle papere',
+                    done: true
+                },
+                {
+                    text: 'Scalare un monte',
+                    done: false
+                },
+                {
+                    text: 'Tuffarsi',
+                    done: false
+                }
+            ]
         }
     }
 }).mount('#app');
